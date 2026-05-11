3a283da938020387fb34c743db1a20700b72689c
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2026-05-11 19:20:32 +0200

    Preparing for pure WPA3 due to fast roaming issues

--- deps.sh
+++ deps.sh
@@ -26,18 +26,18 @@ if [[ $os == "arch" ]]; then
     info "Installing dependencies for Arch Linux"
     sudo pacman -S --needed --noconfirm \
     base-devel ncurses zlib gawk git gettext openssl libxslt wget unzip python \
-    rsync ca-certificates zstd
+    python-distutils-extra rsync ca-certificates zstd perl
 elif [[ $os == "rhel" ]]; then
     info "Installing dependencies for Enterprise Linux"
     sudo dnf install git gawk gettext ncurses-devel zlib-devel \
     openssl-devel libxslt wget which @c-development @development-tools \
-    @development-libs zlib-static python3 zstd
+    @development-libs zlib-static python3 zstd perl perl-IPC-Cmd perl-FindBin
 elif [[ $os == "debian" ]]; then
     info "Installing dependencies for Debian/Ubuntu"
     sudo apt update
     sudo apt install build-essential clang flex bison g++ gawk \
     gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev \
-    python3-setuptools rsync swig unzip zlib1g-dev file wget zstd 
+    python3-setuptools python3-distutils rsync swig unzip zlib1g-dev file wget zstd xsltproc
 fi
 
 ###
